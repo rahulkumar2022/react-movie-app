5f0bd09ac120771fb029f660a49574fb55d5c436
rahulkumar2022 <rahul.kumarcs22@gmail.com>
2025-01-24 02:11:03 +0530

    api integration for serach and favourite movies

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,48 @@
 import MovieCard from "../components/MovieCard";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "../css/Home.css";
+import { getPopularMovies, searchMovies } from "../services/api";
 
 function Home() {
   const [searchQuery, setSearchQuery] = useState("");
-  const movies = [
-    { id: 1, title: "John Wick", release_date: "2020" },
-    { id: 2, title: "Testing movie", release_date: "2024" },
-    { id: 3, title: "Testing movie 3", release_date: "2025" },
-    { id: 4, title: "Testing movie 4", release_date: "2026" },
-    { id: 5, title: "Testing movie 5", release_date: "2027" },
-  ];
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const loadPopularMovies = async () => {
+      try {
+        const popularMovies = await getPopularMovies();
+        setMovies(popularMovies);
+      } catch (err) {
+        setError("Failed to load movie data");
+      } finally {
+        setLoading(false);
+      }
+    };
+    loadPopularMovies();
+  }, []);
+  // const movies = [
+  //   { id: 1, title: "John Wick", release_date: "2020" },
+  //   { id: 2, title: "Testing movie", release_date: "2024" },
+  //   { id: 3, title: "Testing movie 3", release_date: "2025" },
+  //   { id: 4, title: "Testing movie 4", release_date: "2026" },
+  //   { id: 5, title: "Testing movie 5", release_date: "2027" },
+  // ];
 
-  const handleSearch = (e) => {
+  const handleSearch = async (e) => {
     //e.preventDafult();
     e.preventDefault();
-    alert(searchQuery);
+    if (!searchQuery.trim()) return;
+    setLoading(true);
+    try {
+      const searchMovie = await searchMovies(searchQuery);
+      searchMovies(searchMovie);
+      setError(null);
+    } catch (err) {
+      setError("Failed to find movies");
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
@@ -32,14 +59,19 @@ function Home() {
           Search
         </button>
       </form>
-      <div className="movies-grid">
-        {movies.map(
-          (movie) =>
-            movie.title.toLowerCase().startsWith(searchQuery) && (
-              <MovieCard movie={movie} key={movie.id}></MovieCard>
-            )
-        )}
-      </div>
+      {error && <div>{error}</div>}
+      {loading ? (
+        <div className="loading">Loading....</div>
+      ) : (
+        <div className="movies-grid">
+          {movies.map(
+            (movie) =>
+              movie.title.toLowerCase().startsWith(searchQuery) && (
+                <MovieCard movie={movie} key={movie.id}></MovieCard>
+              )
+          )}
+        </div>
+      )}
     </div>
   );
 }
